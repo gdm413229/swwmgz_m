f69a3b1ab88df75d674b5263b7c79265752f53d0
Marisa Kirisame <marisa@sayachan.org>
2020-06-16 16:58:22 +0200

    Looks like this won't be needed

--- lore/mklang.c
+++ lore/mklang.c
@@ -162,7 +162,7 @@ static int ftw_callback( const char *path, const struct stat *st,
 	return FTW_CONTINUE;
 }
 
-#define NLANGS 8
+#define NLANGS 7
 
 int main( void )
 {
@@ -174,8 +174,7 @@ int main( void )
 		"ru",
 		"fr",
 		"it",
-		"de",
-		"esm"
+		"de"
 	};
 	const char langfiles[NLANGS][32] =
 	{
@@ -185,8 +184,7 @@ int main( void )
 		"../language.ru_lore",
 		"../language.fr_lore",
 		"../language.it_lore",
-		"../language.de_lore",
-		"../language.esm_lore",
+		"../language.de_lore"
 	};
 	for ( int i=0; i<NLANGS; i++ )
 	{
